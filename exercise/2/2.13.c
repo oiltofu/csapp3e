2f4afce1d61f7cc1e89b1ce8866334fb69f743fb
oiltofu <ldf9425@163.com>
2023-08-08 08:53:01 +0800

    monarch

---
+++ exercise/2/2.13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int bis(int x, int m);
+int bic(int x, int m);
+
+int bool_or(int x, int y)
+{
+  int result = bis(x, y);
+  return result;
+}
+
+int bool_xor(int x, int y)
+{
+  int result = bis(bic(x, y), bic(y, x));
+  return result;
+}
+
+// 0111  0100
+// 1011  1000
+//       1100
